4f966cd734122b8d30e83f6b9baee2707ab8a643
Gavin Ching <gavinchingy@gmail.com>
2014-12-06 16:25:04 +0200

    Incorporate gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// gulpfile.js
+var gulp = require('gulp');
+var server = require('gulp-express');
+
+gulp.task('server', function () {
+  // Start the server at the beginning of the task
+  server.run({
+    file: './bin/www'
+  });
+
+  // Restart the server when file changes
+  gulp.watch(['app.js', 'routes/**/*.js'], [server.run]);
+});
